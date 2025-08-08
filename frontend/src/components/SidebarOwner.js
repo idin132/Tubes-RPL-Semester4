852cb4a0598b4a3f9b07a5e88d631b48f64786c4
Dwi Putra Juniargi <juniargid@gmail.com>
2025-08-08 23:31:21 +0700

    update sidebar owner namanya ow

--- frontend/src/components/SidebarOwner.js
+++ frontend/src/components/SidebarOwner.js
@@ -6,7 +6,7 @@ const SidebarOwner = () => (
     <div className="sidebar-brand"><i class="fa-solid fa-bars side-icon"></i> DineFlow</div>
     <div className="sidebar-menu">
       <a href="/owner"><i class="fa-solid fa-chart-column side-icon-item"></i> Dashboard</a>
-      <a href="/owner/meja"><i class="fa-solid fa-utensils side-icon-item"></i> Manajemen Meja</a>
+      <a href="/owner/meja"><i class="fa-solid fa-utensils side-icon-item"></i> Lihat Status Meja</a>
       <a href="/owner/riwayat-transaksi"><i class="fa-solid fa-clock-rotate-left side-icon-item"></i> Riwayat Transaksi</a>
       <a href="/owner/menu"><i class="fa-solid fa-list-ul side-icon-item"></i> Daftar Menu</a>
       {/* <a href="#">🔔 Notifikasi Pesanan</a> */}
